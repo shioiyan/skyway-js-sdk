62795eac06cd615d8242167b8ab9192eeebd2d4b
Kanta Matsui <kma9879@gmail.com>
2019-12-21 03:37:15 +0900

    fix

--- dist/skyway.js
+++ dist/skyway.js
@@ -15140,7 +15140,7 @@ class negotiator_Negotiator extends events_default.a {
     logger.log('RTCIceCandidate', candidate);
     logger.log('RTCIceCandidate', new RTCIceCandidate(candidate).toJSON());
     await this._pc
-      .addIceCandidate(candidate)
+      .addIceCandidate(new RTCIceCandidate(candidate))
       .then(() => logger.log('Successfully added ICE candidate'))
       .catch(err => logger.error('Failed to add ICE candidate', err));
   }
